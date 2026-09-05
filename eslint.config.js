9607b97fc85bf733ca688cf4ac48e8e8c9771bd6
HalfSweet <HalfSweet@HalfSweet.cn>
2026-09-05 10:44:13 +0800

    build(deps): add globals for eslint and tokio for the demo runtime

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import prettier from "eslint-config-prettier";
 import vue from "eslint-plugin-vue";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -10,6 +11,12 @@ export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
   ...vue.configs["flat/recommended"],
+  {
+    files: ["src/**/*.{ts,vue}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
   {
     files: ["**/*.vue"],
     languageOptions: {
