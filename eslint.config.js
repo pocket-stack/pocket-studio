5fc815464e1ecb453424e2b5506f5f2a950335b2
HalfSweet <HalfSweet@HalfSweet.cn>
2026-09-06 18:51:26 +0800

    chore(tooling): exclude local design exports from quality checks

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: ["dist", "src-tauri/gen", "src-tauri/target"],
+    ignores: ["dist", "src-tauri/gen", "src-tauri/target", ".design/**"],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
